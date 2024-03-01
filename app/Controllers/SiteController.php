1e2ffdddaa3a86bf5c05a083fec628f6bfe24f73
yxlow07 <m-8717828@moe-dl.edu.my>
2024-03-01 20:16:21 +0800

    Added dynamic navigation bar rendering (highlight on current page, controller set nav items)

--- app/Controllers/SiteController.php
+++ app/Controllers/SiteController.php
@@ -8,8 +8,18 @@ use core\View;
 
 class SiteController extends Controller
 {
+    public function userNavItems(): array
+    {
+        return [
+            '/' => 'Home',
+            '/profile' => 'Profile',
+            '/check_attendance' => 'Check Attendance',
+            '/logout' => 'Logout',
+        ];
+    }
+
     public function render(): void
     {
-        echo View::make()->renderView('index', ['app' => App::$app]);
+        echo View::make()->renderView('index', ['nav' => $this->userNavItems()]);
     }
 }
