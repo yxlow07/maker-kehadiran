564a49f59d75c1f6911ff0f236b459143ebfaa9b
yxlow07 <m-8717828@moe-dl.edu.my>
2024-03-13 19:20:29 +0800

    Minor css updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,17 +6,23 @@ module.exports = {
         './views/layouts/*.twig',
         './views/admin/*.twig',
         './node_modules/preline/dist/*.js',
+        './resources/js/*.js',
     ],
     theme: {
         extend: {
             fontFamily: {
                 chinese: ['Hiragino Sans GB', 'Microsoft YaHei', '微軟正黑體', '蘋果儷中黑', 'SimSun', 'sans-serif'],
-            }
+            },
         },
     },
-    darkMode: 'media',
+    darkMode: 'class',
     plugins: [
         require('@tailwindcss/forms'),
         require('preline/plugin'),
     ],
+    variants: {
+        extend: {
+            display: ['dark'],
+        },
+    },
 }
